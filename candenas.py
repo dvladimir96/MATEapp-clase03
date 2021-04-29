c3e536fce3d112319a2e459c5c990fa81918d99c
dvladimir95 <81610217+dvladimir96@users.noreply.github.com>
2021-04-29 17:26:19 -0500

    Create candenas.py

---
+++ candenas.py
@@ -0,0 +1,17 @@
+#Cadenas de texto
+
+#concatenacion
+a = "uno"
+b = "dos"
+c = a + b
+print(c)
+
+c = a * 3
+print(c) 
+
+#operaciones con variables
+
+a = 14
+b = 12
+c = a + b
+print(a, "hola", b, "=", c)
