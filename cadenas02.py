e5c10d19f0f6c3e1d8cce97a6964a2eb913de8a2
dvladimir95 <81610217+dvladimir96@users.noreply.github.com>
2021-04-29 18:01:29 -0500

    Create cadenas02.py

---
+++ cadenas02.py
@@ -0,0 +1,16 @@
+#concatenacion
+#operaciones con variable y texto
+
+a = 14
+b = 12
+c = a + b
+print(a + b)
+print(a, "hola", b, "=", c)
+#print(a+ "hola"+ b+ "="+ c)    error
+
+#a la hora de concatenar numero y cadenas de texto, es necesario convertir numeros as variables de texto:
+i = "="
+h = "hola"
+print(str(a)+ h + str(b)+ i + str(c))
+
+ 
